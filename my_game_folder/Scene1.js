dad63d6c673e30e4096c5ca2bb0f577263bfa1d4
Ken Otero <keotero99@gmail.com>
2022-10-18 11:47:26 -0400

    Completed Part 5

--- my_game_folder/Scene1.js
+++ my_game_folder/Scene1.js
@@ -5,9 +5,22 @@ class Scene1 extends Phaser.Scene {
 
     preload() {
         this.load.image("background", "/my_game_folder/assets/images/background.png");
-        this.load.image("ship", "/my_game_folder/assets/images/ship.png");
-        this.load.image("ship2", "/my_game_folder/assets/images/ship2.png");
-        this.load.image("ship3", "/my_game_folder/assets/images/ship3.png");
+        this.load.spritesheet("ship", "/my_game_folder/assets/spritesheets/ship.png", {
+            frameWidth: 16,
+            frameHeight: 16
+        });
+        this.load.spritesheet("ship2", "/my_game_folder/assets/spritesheets/ship2.png", {
+            frameWidth: 32,
+            frameHeight: 16
+        });
+        this.load.spritesheet("ship3", "/my_game_folder/assets/spritesheets/ship3.png", {
+            frameWidth: 32,
+            frameHeight: 32
+        });
+        this.load.spritesheet("explosion", "/my_game_folder/assets/spritesheets/explosion.png", {
+            frameWidth: 16,
+            frameHeight: 16
+        });
     }
 
     create() {
